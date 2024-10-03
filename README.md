e2ce802bd47bf24160806583505a6cb558608a57
raocaptmemrao@gmail.com <raocaptmemrao@gmail.com>
2024-10-03 15:16:51 +0500

    ab

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ yarn install
 
 npx sequelize-cli model:generate --name Announcement --attributes name:string
 
+//////for overwrite
+npx sequelize-cli model:generate --name Designation --attributes designation_name:string,description:string,type:string,isDeleted:boolean --force
+
 npx sequelize-cli db:migrate
--- validators/DesignationValidator.js
+++ validators/DesignationValidator.js
@@ -13,9 +13,7 @@ class DesignationValidator extends BaseValidator {
         const schema = Joi.object().keys({
             id: Joi.number().integer().positive().label("ID"),
             designation_name: Joi.string().required().label("Designation Name"),
-            // description: Joi.string().optional().label("Description"),
-            // type: Joi.string().optional().label("Type"),
-            // isDeleted: Joi.boolean().optional().label("Is Deleted"),
+
             createdAt: Joi.date().optional().label("Created At"),
             updatedAt: Joi.date().optional().label("Updated At"),
         });
