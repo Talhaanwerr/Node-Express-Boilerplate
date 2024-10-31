2edcd4500bf7b46f709586eb9ff932d3641501bf
Memona <raocaptmemrao@gmail.com>
2024-10-31 01:28:12 -0700

    leave+v

--- validators/LeaveValidator.js
+++ validators/LeaveValidator.js
@@ -1,25 +1,24 @@
-function validateCreateLeave(data) {
-    if (!data.type_of_leave) {
-      return { status: false, message: "Leave type is required" };
-    }
-    if (typeof data.available !== "number" || data.available < 0) {
-      return { status: false, message: "Available leaves must be a positive number" };
-    }
-    return { status: true };
-  }
-  
-  function validateUpdateLeave(data) {
-    if (data.available && typeof data.available !== "number") {
-      return { status: false, message: "Available leaves must be a number" };
-    }
-    if (data.used && typeof data.used !== "number") {
-      return { status: false, message: "Used leaves must be a number" };
-    }
-    return { status: true };
-  }
-  
-  module.exports = {
-    validateCreateLeave,
-    validateUpdateLeave,
+const Joi = require("joi");
+const BaseValidator = require("./BaseValidator.js");
+
+class LeaveValidator extends BaseValidator {
+  validateCreateLeave = (data) => {
+    const schema = Joi.object({
+      type_of_leave: Joi.string().required().label("Leave Type"),
+      available: Joi.number().min(0).required().label("Available Leaves"),
+    });
+
+    return this.validate(schema, data);
   };
-  
+
+  validateUpdateLeave = (data) => {
+    const schema = Joi.object({
+      available: Joi.number().optional().label("Available Leaves"),
+      used: Joi.number().optional().label("Used Leaves"),
+    });
+
+    return this.validate(schema, data);
+  };
+}
+
+module.exports = new LeaveValidator();
